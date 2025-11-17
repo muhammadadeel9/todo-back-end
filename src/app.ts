b65c37dce2afb419f8568f538e261fd69813327c
Muhammad Adeel <sumaaeel@gmail.com>
2025-11-17 20:51:44 +0500

    some changes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
 import DbConnection from "./config/dbconfig";
 import router from "./routes/index";
-export const app = express();
-const PORT = process.env.PORT || 3200;
+import ServerlessHttp from "serverless-http";
+
+const app = express();
+// const PORT = process.env.PORT || 3200;
 // Middleware
 DbConnection();
 app.use(express.json());
@@ -10,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+exports.handler = ServerlessHttp(app);
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
