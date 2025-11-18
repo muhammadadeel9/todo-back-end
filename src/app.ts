8298b59201093abc7837c448cc84fbead72c38a3
Muhammad Adeel <sumaaeel@gmail.com>
2025-11-18 10:30:06 +0500

    some changes

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ export  const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api" ,router);
+app.use("/" ,router);
 
 // app.listen(PORT, () => {
 //   console.log(`Server is running on http://localhost:${PORT}`);
