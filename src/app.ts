3a93e3c957389930f5f029e1c1c4b44293ef58be
Muhammad Adeel <sumaaeel@gmail.com>
2025-12-18 10:26:13 +0500

    cors

--- src/app.ts
+++ src/app.ts
@@ -4,23 +4,27 @@ import router from "./routes/index";
 import { swaggerSpec } from "./config/swagger";
 import swaggerUi from "swagger-ui-express";
 import cors from 'cors';
-export  const app = express();
+export const app = express();
 const PORT = process.env.PORT || 3200;
 
 
 async function bootstrap() {
-    await DbConnection();
+  await DbConnection();
 }
 
 bootstrap();
+// Source - https://stackoverflow.com/a
+// Posted by Sohail Ahmad, modified by community. See post 'Timeline' for change history
+// Retrieved 2025-12-18, License - CC BY-SA 4.0
 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
+});
 
-app.use(cors());
-app.use(cors({
-  origin: [process.env.FRONT_END_URI as string || "", process.env.LOCAL_URL as string || ""],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
 app.use(express.json());
 app.use(
   "/api-docs",
@@ -35,7 +39,7 @@ app.get("/api-docs.json", (req: Request, res: Response) => {
   res.setHeader("Content-Type", "application/json");
   res.send(swaggerSpec);
 });
-app.use("/api" ,router);
+app.use("/api", router);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
